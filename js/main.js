f2a8555a8ceb317a1ab1a1f7deec89363a37aac6
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-27 08:54:46 -0500

    feat: :construction: Borrar json

--- js/main.js
+++ js/main.js
@@ -35,10 +35,26 @@ const editProduct = async(data) =>{
 while (confirm("¿ Deseas actualizar un producto?")){
     const data = {}
     data.id = prompt("Ingrese el id del producto a actualizar", "Mic330")
-    data.name = (confirm("¿Desea actualizar el nonmbre del prudcto ?")) ? prompt("Ingrese el nonbre del producto") : undefined
+    data.name = (confirm("¿Desea actualizar el nonmbre del producto ?")) ? prompt("Ingrese el nonbre del producto") : undefined
     if(confirm("¿Desea actulizar el precio del producto?")) data.price = Number(prompt("Ingrese el nuevo precio del producto"))
     if(confirm("¿Desea actualizar la categoria del producto?")) data.category = prompt("Ingrese la nueva categoria del producto")
     editProduct(data)
     .then(result => alert(JSON.stringify(result)))
     .catch(error => alert(error))
+}
+
+const deleteProduct = async(data) =>{
+    const {id} = data
+    const config ={
+        method: "DELETE",
+    }
+    const reponse = await fetch(`http://127.0.0.1:5600/products${id}`,config)
+    const result = await reponse.json()
+    return result
+}
+
+while(confirm("¿ Deseas eliminar un producto?")){
+    const data = {}
+    data.id = prompt("Ingrese la id del producto que desea eliminar", "Mic330")
+    data.name = (confirm("¿Desea eliminar el nombre del producto")) ? (alert("Nom"))
 }
